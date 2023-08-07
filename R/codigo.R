f25a5293ef57c6c1fa2d1ff8ce2fe392a77c8e9e
eogasawara <eogasawara@eic.cefet-rj.br>
2023-08-07 09:05:09 -0300

    Update

--- R/codigo.R
+++ R/codigo.R
@@ -15,19 +15,19 @@ summary(dataset)
 plot_ts(x = 1:length(dataset$x), y = dataset$x)
 plot_ts(x = 1:length(dataset$y), y = dataset$y)
 
-
-write.table(dataset, file="dataset.csv", row.names=FALSE, quote = FALSE, sep = ",")
-
+write.table(dataset, file="dataset.csv", row.names=FALSE, col.names = FALSE, quote = FALSE, sep = ",")
 
 
 datax <- as.data.frame(ts_data(dataset$x, 5))
-colnames(datax) <- c("x4", "x3", "x2", "x1", "x0")
-write.table(datax, file="datax.csv", row.names=FALSE, quote = FALSE, sep = ",")
+head(datax)
+write.table(datax, file="datax.csv", row.names=FALSE, col.names = FALSE, quote = FALSE, sep = ",")
 
 
 datay <- as.data.frame(ts_data(dataset$y, 5))
-colnames(datay) <- c("y4", "y3", "y2", "y1", "y0")
-write.table(datay, file="datay.csv", row.names=FALSE, quote = FALSE, sep = ",")
+head(datay)
+write.table(datay, file="datay.csv", row.names=FALSE, col.names = FALSE, quote = FALSE, sep = ",")
 
 
 dataxy <- cbind(datax, datay)
+head(dataxy)
+write.table(dataxy, file="dataxy.csv", row.names=FALSE, col.names = FALSE, quote = FALSE, sep = ",")
